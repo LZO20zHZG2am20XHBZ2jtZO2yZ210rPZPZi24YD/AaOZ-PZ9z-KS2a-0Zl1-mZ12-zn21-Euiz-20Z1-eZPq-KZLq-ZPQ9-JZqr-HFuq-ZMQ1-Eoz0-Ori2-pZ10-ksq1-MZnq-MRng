33070a9047aa471af22958612b8f803141f6b792
LZO20zHZG2am20XHBZ2jtZO2yZ210rPZPZi24YD <109888411+LZO20zHZG2am20XHBZ2jtZO2yZ210rPZPZi24YD@users.noreply.github.com>
2022-07-24 01:49:43 -0300

    Create DOzq-PEz0-20ZA-zm2Z-Lw1O-ZPqir-JZqh-ZHq2-KLTR-ZmqE-iq29-9882-zqEr-OZ01-ZPR8-z721-jZB2-oQ9-zMQz-J28Z-jkqE-kz01-FpqZ-o29z-91Sr-Eq9Z-TqIe-02tE-90zO-oZOq-SNAV-lo29-SU1t-P120-MA01-nAEG-ni15-G01-OZeo-550L-oq014.lua

---
+++ DOzq-PEz0-20ZA-zm2Z-Lw1O-ZPqir-JZqh-ZHq2-KLTR-ZmqE-iq29-9882-zqEr-OZ01-ZPR8-z721-jZB2-oQ9-zMQz-J28Z-jkqE-kz01-FpqZ-o29z-91Sr-Eq9Z-TqIe-02tE-90zO-oZOq-SNAV-lo29-SU1t-P120-MA01-nAEG-ni15-G01-OZeo-550L-oq014.lua
@@ -0,0 +1,62 @@
+
+-- Teleport script for jailbreak
+
+local Player = game.Players.LocalPlayer
+local Backpack = Player.Backpack
+local Character = Player.Character or Player.CharacterAdded:Wait()
+local Point = Character:WaitForChild("HumanoidRootPart")
+local TweenService = game:GetService("TweenService")
+
+local function Notification(Title,Desc) 
+local CoreGui = game:GetService("CoreGui") local CorePackages = game:GetService("CorePackages") local UIBlox = getrenv().require(CorePackages.UIBlox) UIBlox.init() local StylePalette = getrenv().require(CorePackages.AppTempCommon.LuaApp.Style.StylePalette) local stylePalette = StylePalette.new() stylePalette:updateTheme("dark") stylePalette:updateFont("gotham") local Roact = getrenv().require(CorePackages.Roact) local Images = getrenv().require(CorePackages.Packages._Index.UIBlox.UIBlox.App.ImageSet.Images) local Gui =Roact.createElement("ScreenGui",{IgnoreGuiInset = true, ZIndexBehavior = Enum.ZIndexBehavior.Global},{Gui = Roact.createElement(UIBlox.Core.Style.Provider,{style = stylePalette},{Toast = Roact.createElement(UIBlox.App.Dialog.Toast,{toastContent = {toastTitle = Title,toastSubtitle = Desc,iconImage = Images["icons/menu/trade"]}})})}) Roact.mount(Gui, CoreGui, "Notification")
+task.wait(3)
+game:GetService("CoreGui"):WaitForChild("Notification"):Destroy()
+end
+
+
+local function AirStuck()
+Point.Anchored = false
+end
+
+local function Bypass()
+    coroutine.wrap(Notify)("Teleporting!","Please wait.")
+    for i = 1,15 do
+        if Backpack:FindFirstChild("Punch") then
+        Character:WaitForChild("Humanoid"):EquipTool(Backpack.Punch)
+        elseif Character:FindFirstChild("Punch") then
+        Character:WaitForChild("Humanoid"):UnequipTools()
+        else
+        coroutine.wrap(Notify)("Failed!","You must have the punch tool to finish teleporting.")
+        break
+        end
+        task.wait(0.07)
+    end
+end
+
+function TP(Player)
+    if not game.Players:FindFirstChild(Player) then
+    coroutine.wrap(Notify)("Failed!","Player is either invalid or has left the game.")
+    elseif game.Players:FindFirstChild(Player) then
+    Character.Humanoid.Jump = true
+    local Target = game.Players:WaitForChild(Player).Character:WaitForChild("HumanoidRootPart").CFrame
+    coroutine.wrap(Bypass)()
+    TweenService:Create(Point, TweenInfo.new(1.5, Enum.EasingStyle.Linear), {CFrame = Target}):Play()
+    task.wait(1.4)
+    Point.Anchored = true
+    task.wait(1.2)
+    Point.Anchored = false
+    Point.CFrame = Point.CFrame - Vector3.new(0,-10,0)
+    end
+end
+
+function TPP(X,Y,Z)
+    Character.Humanoid.Jump = true
+    local Target = CFrame.new(X,Y,Z)
+    coroutine.wrap(Bypass)()
+    TweenService:Create(Point, TweenInfo.new(1.5, Enum.EasingStyle.Linear), {CFrame = Target}):Play()
+    task.wait(1.4)
+    Point.Anchored = true
+    task.wait(1.2)
+    Point.Anchored = false
+    Point.CFrame = Point.CFrame - Vector3.new(0,-10,0)
+end
